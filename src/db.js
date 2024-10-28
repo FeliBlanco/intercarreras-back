f2768f7ff35540db5e4cf63249ce629ef313759a
feliblanco <blancofeli9@gmail.com>
2024-10-28 00:06:58 -0300

    cambios

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/pou');
+    await mongoose.connect('mongodb+srv://feliblanco:feliblanco123@cluster0.h0ir0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MONGO DB CONECTADO!')
 }
 
